8deacdfee8ccf94c017d45ce891e5a8fe20285d2
Dinesh-9025 <dineshcareer4@gmail.com>
2023-07-06 13:22:56 +0530

    nx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-
+import 'tailwindcss/tailwind.css';
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
-    <div>
+    <div className="text-3xl font-bold underline">
       <NxWelcome title="org" />
     </div>
   );
